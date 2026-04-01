d91e250852a5b2b910adfa8f23b2dbd7f8ae5219
Divakar <trdiva2608@gmail.com>
2026-04-01 22:28:21 +0530

    Built UserService endpoints, Repository Layer Created, Removed lombok and added the required constructor, getter and setters for the class methods.

--- userservice/src/main/java/com/fitness/userservice/dto/RegisterRequest.java
+++ userservice/src/main/java/com/fitness/userservice/dto/RegisterRequest.java
@@ -3,9 +3,7 @@ package com.fitness.userservice.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 
-@Data
 public class RegisterRequest {
 
     @NotBlank(message = "Email is required")
@@ -17,5 +15,57 @@ public class RegisterRequest {
     private String password;
     private String firstName;
     private String lastName;
+
+    public RegisterRequest() {
+    }
+
+    public RegisterRequest(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisterRequest{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 
 }
