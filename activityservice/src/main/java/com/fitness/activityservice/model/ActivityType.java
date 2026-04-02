1802eb2431dcb69c605456b17cfa3565a2d5af71
Divakar <trdiva2608@gmail.com>
2026-04-02 21:48:10 +0530

    MongoDB Initial Setup and Database connection establishment.

---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,15 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WIGHT_TRAINING,
+    YOGA,
+    CARDIO,
+    STRETCHING,
+    OTHER
+
+}
