b2522d0cc89522af6fd2397d9210965a45acc0a7
Divakar <trdiva2608@gmail.com>
2026-04-05 13:11:47 +0530

    Integrated Gemini API in AIService for generating recommendations based on the activity.

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -7,9 +7,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ActivityMessageListener {
 
+    private final ActivityAIService aiService;
+
+    public ActivityMessageListener(ActivityAIService aiService) {
+        this.aiService = aiService;
+    }
+
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity) {
         System.out.println("Received activity for processing : " + activity.getId());
+        System.out.println("Response from Gemini AI : " + aiService.generateRecommendations(activity));
     }
 
 }
