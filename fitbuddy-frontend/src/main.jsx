ed6860a613a65dbde55c8669e715e2ce736899e9
Divakar <trdiva2608@gmail.com>
2026-04-10 23:11:38 +0530

    Built PKCE Authorization flow using react-oauth2-code-pkce

--- fitbuddy-frontend/src/main.jsx
+++ fitbuddy-frontend/src/main.jsx
@@ -4,11 +4,17 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 
+import { authConfig } from './authConfig'
+import { AuthProvider } from 'react-oauth2-code-pkce'
+
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <AuthProvider authConfig={authConfig}
+                loadingComponent = {<div>Loading...</div>}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AuthProvider>,
 )
