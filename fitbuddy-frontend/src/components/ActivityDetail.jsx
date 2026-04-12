59038da0394e299c45a75b08c0bdf0029d471d95
Divakar <trdiva2608@gmail.com>
2026-04-12 13:47:13 +0530

    Built Activity List and Activity Detail Page to display the activities and AI Recommendations fetched from the backend and done some UI changes for login and logout

--- fitbuddy-frontend/src/components/ActivityDetail.jsx
+++ fitbuddy-frontend/src/components/ActivityDetail.jsx
@@ -1,10 +1,79 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router'
+import { getActivityDetail } from '../services/api';
+import { Box, Card, CardContent, Divider, Typography } from '@mui/material';
 
 const ActivityDetail = () => {
+
+  const { id } = useParams();
+  const [activity, setActivity] = useState([]);
+  const [recommendation, setRecommendation] = useState([]);
+
+  const fetchActivityDetail = async () => {
+      try {
+        const response = await getActivityDetail(id);
+        setActivity(response.data);
+        setRecommendation(response.data.recommendation);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+  useEffect(() => {
+    fetchActivityDetail();
+  }, [id]);
+
+  if (!activity) {
+    return <Typography>Loading...</Typography>
+  }
+
   return (
-    <div>
-      ActivityDetail
-    </div>
+    <Box sx={{ maxwidth: 800, mx: 'auto', p: 2 }}>
+
+      <Card sx={{ mb: 2 }}>
+        <CardContent>
+          <Typography variant="h5" gutterBottom>Activity Details</Typography>
+          <Typography>Type : {activity.activityType}</Typography>
+          <Typography>Date : {new Date(activity.createdAt).toLocaleString()}</Typography>
+        </CardContent>
+      </Card>
+
+      {
+        recommendation && (
+          <Card>
+            <CardContent>
+
+              <Typography variant="h5" gutterBottom>AI Recommendation</Typography>
+              <Typography variant="h6" gutterBottom>Analysis</Typography>
+              <Typography paragraph>{activity.recommendation}</Typography>
+
+              <Divider sx={{ my: 2 }}></Divider>
+
+              <Typography variant="h5">Improvements</Typography>
+              {activity?.improvements?.map((improvement, index) => (
+                <Typography key={index}>• {improvement}</Typography>
+              ))}
+
+              <Divider sx={{ my: 2 }}></Divider>
+
+              <Typography variant="h5">Suggestions</Typography>
+              {activity?.suggestions?.map((suggestion, index) => (
+                <Typography key={index}>• {suggestion}</Typography>
+              ))}
+
+              <Divider sx={{ my: 2 }}></Divider>
+
+              <Typography variant="h5">Safety Guidelines</Typography>
+              {activity?.safety?.map((safety, index) => (
+                <Typography key={index}>• {safety}</Typography>
+              ))}
+
+            </CardContent>
+          </Card>
+        )
+      }
+
+    </Box>
   )
 }
 
